878400ce6ac0e2ca99be8816408ac6cf91db8016
Pratham Jain <2muchappy420@gmail.com>
2025-08-30 13:56:37 +0530

    mongo setup

---
+++ push_data.py
@@ -0,0 +1,11 @@
+from pymongo.mongo_client import MongoClient
+
+uri = ""
+
+client = MongoClient(uri)
+
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
