bddc9df3b949088f207e675e4b45cd134bc88183
Pratham Jain <2muchappy420@gmail.com>
2025-09-14 00:56:50 +0530

    data validation; schema.yaml created

--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -25,3 +25,14 @@ FILE_NAME = "NetworkData.csv"
 
 TRAIN_FILE_NAME= "train.csv"
 TEST_FILE_NAME= "test.csv"
+
+SCHEMA_FILE_PATH = os.path.join("data_schema", "schema.yaml")
+
+"""
+Data Validation related constant start with DATA_VALIDATION VAR NAME
+"""
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+DATA_VALIDATION_VALID_DIR: str = "validated"
+DATA_VALIDATION_INVALID_DIR: str = "invalid"
+DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
