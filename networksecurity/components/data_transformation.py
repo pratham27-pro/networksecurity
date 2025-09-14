07161694a87707e6d0d4a70ae0b91c1a754dcb29
Pratham Jain <2muchappy420@gmail.com>
2025-09-15 01:43:27 +0530

    data transformation architecture

---
+++ networksecurity/components/data_transformation.py
@@ -0,0 +1,19 @@
+import sys
+import os
+import numpy
+import pandas as pd
+
+from sklearn.impute import KNNImputer
+from sklearn.pipeline import Pipeline
+
+from networksecurity.constant.training_pipeline import TARGET_COLUMN, DATA_TRANSFORMATION_IMPUTER_PARAM
+
+from networksecurity.entity.artifact_entity import (
+    DataTransformationArtifact,
+    DataValidationArtifact
+)
+
+from networksecurity.entity.config_entity import DataTransformationConfig
+from networksecurity.exception.exception import NetworkSecurityException
+from networksecurity.logging.logger import logging
+from networksecurity.utils.main_utils.utils import  save_numpy_array_data, save_object
