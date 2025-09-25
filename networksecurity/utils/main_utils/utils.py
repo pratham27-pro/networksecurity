0272c8d2648109fbe5455ee1f760d7b2fac359f4
Pratham Jain <2muchappy420@gmail.com>
2025-09-26 00:52:54 +0530

    model trainer and evaluation

--- networksecurity/utils/main_utils/utils.py
+++ networksecurity/utils/main_utils/utils.py
@@ -7,6 +7,9 @@ import numpy as np
 import dill
 import pickle
 
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import r2_score
+
 def read_yaml_file(file_path: str) -> dict:
     try:
         with open(file_path, "rb") as yaml_file:
@@ -70,5 +73,33 @@ def load_numpy_array_data(file_path: str) -> np.array:
     try:
         with open(file_path, "rb") as file_obj:
             return np.load(file_obj)
+    except Exception as e:
+        raise NetworkSecurityException(e, sys) from e
+    
+def evaluate_models(X_train, y_train, x_test, y_test, models, param):
+    try:
+        report = {}
+
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            para = param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model.para, cv=3)
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train, y_train)
+
+            # model.fit(X_train, y_train)
+
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(x_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
     except Exception as e:
         raise NetworkSecurityException(e, sys) from e
